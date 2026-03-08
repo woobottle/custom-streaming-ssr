7c1461efcf6f430f170beeb8bc15ace506e55be2
wooBottle <dkrnfls@gmail.com>
2026-03-08 19:02:06 +0900

    add build options and entry-server

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist/client'
+export default defineConfig(({ isSsrBuild }: ConfigEnv) => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    server: {
+      port: 3000,
+      ssr: isSsrBuild,
+    },
+    build: {
+      outDir: isSsrBuild ? 'dist/server' : 'dist/client',
+    }
   }
 })
